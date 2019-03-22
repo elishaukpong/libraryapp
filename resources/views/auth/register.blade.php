e1504742f2dbf8082139ff4ee1207dac6dd107c6
drumzminister <ishukpong418@gmail.com>
2019-03-22 02:57:35 +0100

    auth ui fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="jumbotron text-center my-4">
+    <h1>Register</h1>
+</div>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
@@ -63,7 +63,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary form-control">
                                     {{ __('Register') }}
                                 </button>
                             </div>
@@ -73,5 +73,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
